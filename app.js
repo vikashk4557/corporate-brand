3e473202378898e5be4c9618dc9ec6435c4edbc8
Vikash Kumar <vikashkumar998475@gmail.com>
2026-08-08 15:12:20 +0530

    fix: root route

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+    res.redirect("/brands");
+});
+
 app.get("/brands", (req, res) => {
     res.render("brands/index");
 });
